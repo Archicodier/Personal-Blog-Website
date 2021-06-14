ae84eab12fda0a0c681b27f4153483728bd22ec1
Calvin Hu <31859826+chu017@users.noreply.github.com>
2021-06-14 00:28:58 -0700

    projects

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,50 +1,27 @@
-import Link from 'next/link'
-import { GetStaticProps } from 'next'
-import utilStyles from '../../styles/utils.module.css'
-import blogStyles from '../../styles/blog.module.css'
-import { getSortedPostsData } from '../../lib/posts'
-import Date from '../../components/date'
 import { Navbar } from '../../components/navbar'
+import utilStyles from '../../styles/utils.module.css'
+import projectsStyles from '../../styles/projects.module.css'
+import Lists from '../../projects/lists'
 
-export default function Posts({
-  allPostsData
-}: {
-  allPostsData: {
-    date: string
-    title: string
-    id: string
-  } []
-}) {
+export default function Projects() {
   return (
     <>
     <Navbar />
-    <div className={blogStyles.container}>
+    <div className={projectsStyles.container}>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Projects</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/blog/${id}`}>
-               <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-               <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+        <div className={projectsStyles.section}>
+          <img className={projectsStyles.image} src={Lists.imageURL as string} />
+          <div>
+           <div className={projectsStyles.headingLg}>{Lists.name as string}</div>
+           <div className={projectsStyles.a}>
+            <a className={projectsStyles.headingSsm} href={Lists.url as string}>github/healthy-food-inc</a>
+           </div>
+           <div className={projectsStyles.headingSm}>{Lists.summary as string}</div>
+          </div>
+        </div>
       </section>
     </div>
     </>
   )
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
 }
---
+++ projects/lists.js
@@ -0,0 +1,9 @@
+var lists =
+  {
+    name: 'Healthy Food Inc',
+    imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP2xZvNamKoPteZ1padSadP2MuWjtPs2TVv2dg0tlUKZnrTxfdpCfObsWzZKH9t6rYXfA&usqp=CAU',
+    url: 'https://github.com/Health-Mart/healthy-food-inc',
+    summary: 'Healthy Food Inc. is a meal planning, recipe generating, and grocery shopping platform where users can purchase healthy products from local producers'
+  }
+
+export default lists
