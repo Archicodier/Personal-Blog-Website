63058fdc2eef7c385fa66d520dc9c52d9e699b14
Calvin Hu <31859826+chu017@users.noreply.github.com>
2021-06-14 01:04:31 -0700

    revise projects list

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -11,13 +11,33 @@ export default function Projects() {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Projects</h2>
         <div className={projectsStyles.section}>
-          <img className={projectsStyles.image} src={Lists.imageURL as string} />
-          <div>
-           <div className={projectsStyles.headingLg}>{Lists.name as string}</div>
+          <img className={projectsStyles.image} src={Lists[0].imageURL as string} />
+          <div className={projectsStyles.division}>
+           <a className={projectsStyles.headingLg} href={Lists[0].url as string}>{Lists[0].name as string}</a>
            <div className={projectsStyles.a}>
-            <a className={projectsStyles.headingSsm} href={Lists.url as string}>github/healthy-food-inc</a>
+            <a className={projectsStyles.headingSsm} href={Lists[0].url as string}>github/healthy-food-inc</a>
            </div>
-           <div className={projectsStyles.headingSm}>{Lists.summary as string}</div>
+           <div className={projectsStyles.headingSm}>{Lists[0].summary as string}</div>
+          </div>
+        </div>
+        <div className={projectsStyles.section}>
+          <img className={projectsStyles.image} src={Lists[1].imageURL as string} />
+          <div className={projectsStyles.division}>
+          <a className={projectsStyles.headingLg} href={Lists[1].url as string}>{Lists[1].name as string}</a>
+           <div className={projectsStyles.a}>
+            <a className={projectsStyles.headingSsm} href={Lists[1].url as string}>github/project-catwalk</a>
+           </div>
+           <div className={projectsStyles.headingSm}>{Lists[1].summary as string}</div>
+          </div>
+        </div>
+        <div className={projectsStyles.section}>
+          <img className={projectsStyles.image} src={Lists[1].imageURL as string} />
+          <div className={projectsStyles.division}>
+          <a className={projectsStyles.headingLg} href={Lists[1].url as string}>{Lists[1].name as string}</a>
+           <div className={projectsStyles.a}>
+            <a className={projectsStyles.headingSsm} href={Lists[1].url as string}>github/project-catwalk</a>
+           </div>
+           <div className={projectsStyles.headingSm}>{Lists[1].summary as string}</div>
           </div>
         </div>
       </section>
--- projects/lists.js
+++ projects/lists.js
@@ -1,9 +1,23 @@
 var lists =
+[
   {
     name: 'Healthy Food Inc',
-    imageURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP2xZvNamKoPteZ1padSadP2MuWjtPs2TVv2dg0tlUKZnrTxfdpCfObsWzZKH9t6rYXfA&usqp=CAU',
+    imageURL: 'https://github.com/Health-Mart/healthy-food-inc/blob/main/GIFs/Recipe_Generator.gif?raw=true',
     url: 'https://github.com/Health-Mart/healthy-food-inc',
     summary: 'Healthy Food Inc. is a meal planning, recipe generating, and grocery shopping platform where users can purchase healthy products from local producers'
+  },
+  {
+    name: 'Project Catwalk',
+    imageURL: 'https://cdn.downtoearth.org.in/library/medium/2016-09-22/0.11503100_1474548006_zebra.jpg',
+    url: 'https://github.com/Weyland-Software-Group/fec',
+    summary: 'Project Catwalk is a project by the Weyland Software Group to take a set of data from an API and render it onto a beautiful product page.'
+  },
+  {
+    name: 'Project Catwalk',
+    imageURL: 'https://cdn.downtoearth.org.in/library/medium/2016-09-22/0.11503100_1474548006_zebra.jpg',
+    url: 'https://github.com/Weyland-Software-Group/fec',
+    summary: 'Project Catwalk is a project by the Weyland Software Group to take a set of data from an API and render it onto a beautiful product page.'
   }
+]
 
 export default lists
