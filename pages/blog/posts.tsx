7a2aafb26ace32ac4de33a11a2ac9591999ec4bd
Calvin Hu <31859826+chu017@users.noreply.github.com>
2021-06-14 22:51:56 -0700

    header for projects & blog

--- pages/blog/posts.tsx
+++ pages/blog/posts.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import { GetStaticProps } from 'next'
 import utilStyles from '../../styles/utils.module.css'
 import blogStyles from '../../styles/blog.module.css'
+import styles from '../../components/layout.module.css'
 import { getSortedPostsData } from '../../lib/posts'
 import Date from '../../components/date'
 import { Navbar } from '../../components/navbar'
@@ -20,20 +21,25 @@ export default function Posts({
     <Navbar />
     <div className={blogStyles.container}>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/blog/${id}`}>
-               <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-               <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+        <header className={styles.header}>
+           <div className={utilStyles.headingXl}>Blog</div>
+           <div className={utilStyles.headingLg}>Writing about tech and beyond</div>
+        </header>
+        <main className={styles.main}>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+             <li className={utilStyles.listItem} key={id}>
+               <Link href={`/blog/${id}`}>
+                 <a>{title}</a>
+                </Link>
+               <br />
+               <small className={utilStyles.lightText}>
+                 <Date dateString={date} />
+                </small>
+              </li>
+           ))}
+         </ul>
+        </main>
       </section>
     </div>
     </>
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from '../../components/navbar'
 import utilStyles from '../../styles/utils.module.css'
+import styles from '../../components/layout.module.css'
 import projectsStyles from '../../styles/projects.module.css'
 import Lists from '../../projects/lists'
 
@@ -9,37 +10,42 @@ export default function Projects() {
     <Navbar />
     <div className={projectsStyles.container}>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Projects</h2>
-        <div className={projectsStyles.section}>
-          <img className={projectsStyles.image} src={Lists[0].imageURL as string} />
-          <div className={projectsStyles.division}>
-           <a className={projectsStyles.headingLg} href={Lists[0].url as string}>{Lists[0].name as string}</a>
-           <div className={projectsStyles.a}>
-            <a className={projectsStyles.headingSsm} href={Lists[0].url as string}>github/healthy-food-inc</a>
-           </div>
-           <div className={projectsStyles.headingSm}>{Lists[0].summary as string}</div>
+        <header className={styles.header}>
+          <div className={utilStyles.headingXl}>Projects</div>
+          <div className={utilStyles.headingLg}>Full Stack Web Projects</div>
+        </header>
+        <main className={styles.main}>
+          <div className={projectsStyles.section}>
+           <img className={projectsStyles.image} src={Lists[0].imageURL as string} />
+           <div className={projectsStyles.division}>
+            <a className={projectsStyles.headingLg} href={Lists[0].url as string}>{Lists[0].name as string}</a>
+             <div className={projectsStyles.a}>
+              <a className={projectsStyles.headingSsm} href={Lists[0].url as string}>github/healthy-food-inc</a>
+             </div>
+             <div className={projectsStyles.headingSm}>{Lists[0].summary as string}</div>
+            </div>
+         </div>
+          <div className={projectsStyles.section}>
+            <img className={projectsStyles.image} src={Lists[1].imageURL as string} />
+            <div className={projectsStyles.division}>
+           <a className={projectsStyles.headingLg} href={Lists[1].url as string}>{Lists[1].name as string}</a>
+            <div className={projectsStyles.a}>
+              <a className={projectsStyles.headingSsm} href={Lists[1].url as string}>github/project-catwalk</a>
+             </div>
+             <div className={projectsStyles.headingSm}>{Lists[1].summary as string}</div>
+            </div>
           </div>
-        </div>
-        <div className={projectsStyles.section}>
-          <img className={projectsStyles.image} src={Lists[1].imageURL as string} />
-          <div className={projectsStyles.division}>
-          <a className={projectsStyles.headingLg} href={Lists[1].url as string}>{Lists[1].name as string}</a>
-           <div className={projectsStyles.a}>
-            <a className={projectsStyles.headingSsm} href={Lists[1].url as string}>github/project-catwalk</a>
-           </div>
-           <div className={projectsStyles.headingSm}>{Lists[1].summary as string}</div>
+          <div className={projectsStyles.section}>
+            <img className={projectsStyles.image} src={Lists[2].imageURL as string} />
+            <div className={projectsStyles.division}>
+            <a className={projectsStyles.headingLg} href={Lists[2].url as string}>{Lists[2].name as string}</a>
+            <div className={projectsStyles.a}>
+             <a className={projectsStyles.headingSsm} href={Lists[2].url as string}>github/project-catwalk</a>
+            </div>
+             <div className={projectsStyles.headingSm}>{Lists[2].summary as string}</div>
+            </div>
           </div>
-        </div>
-        <div className={projectsStyles.section}>
-          <img className={projectsStyles.image} src={Lists[2].imageURL as string} />
-          <div className={projectsStyles.division}>
-          <a className={projectsStyles.headingLg} href={Lists[2].url as string}>{Lists[2].name as string}</a>
-           <div className={projectsStyles.a}>
-            <a className={projectsStyles.headingSsm} href={Lists[2].url as string}>github/project-catwalk</a>
-           </div>
-           <div className={projectsStyles.headingSm}>{Lists[2].summary as string}</div>
-          </div>
-        </div>
+        </main>
       </section>
     </div>
     </>
